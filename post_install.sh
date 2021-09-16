2e54f6b871f7917abf44817c07722b917ae727e1
root <root@truenas.local>
2021-09-16 17:13:08 -0400

    adding auto generate password

--- post_install.sh
+++ post_install.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
 sysrc -f /etc/rc.conf adguardhome_enable="YES"
+
+password=$(openssl rand -base64 15)
+
+passwordagh=$(htpasswd -b -n -B -C 10 '' $password | tr -d ':')
+
+sed -i 's/  password:/  password: $passwordagh/g' /usr/local/bin/AdGuardHome.yaml
+
+echo "User Name: admin" > /root/PLUGIN_INFO
+echo "Password: $password" > /root/PLUGIN_INFO
+
 service adguardhome start > /dev/null
