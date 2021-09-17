d19660fe366986d3f6d1e7e217be29d1cfe3a7a9
root <root@truenas.local>
2021-09-17 15:04:09 -0400

    update plugin_info

--- post_install.sh
+++ post_install.sh
@@ -2,7 +2,18 @@
 
 sysrc -f /etc/rc.conf adguardhome_enable="YES"
 
-echo "User Name: admin" > /root/PLUGIN_INFO
-echo "Password: admin" > /root/PLUGIN_INFO
-
 service adguardhome start > /dev/null
+
+echo
+cat <<EOF > /root/PLUGIN_INFO
+#---------------------------------------------------------------------#
+# Getting started with the MineOs plugin
+#---------------------------------------------------------------------#
+AdGuard Home is a network-wide software for blocking ads & tracking. 
+After you set it up, it'll cover ALL your home devices, 
+and you don't need any client-side software for that.
+Source: https://github.com/AdguardTeam/AdGuardHome
+ 
+The default user for the Admin Portal is "admin" with password "admin"
+
+EOF
