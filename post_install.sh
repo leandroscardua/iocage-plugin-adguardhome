4556b50efc4a90424df0585578532a05580d8cdd
root <root@truenas.local>
2021-09-17 10:10:39 -0400

    adding pluing information

--- post_install.sh
+++ post_install.sh
@@ -2,13 +2,7 @@
 
 sysrc -f /etc/rc.conf adguardhome_enable="YES"
 
-password=$(openssl rand -base64 15)
-
-passwordagh=$(htpasswd -b -n -B -C 10 '' $password | tr -d ':')
-
-sed -i 's/  password:/  password: $passwordagh/g' /usr/local/bin/AdGuardHome.yaml
-
 echo "User Name: admin" > /root/PLUGIN_INFO
-echo "Password: $password" > /root/PLUGIN_INFO
+echo "Password: admin" > /root/PLUGIN_INFO
 
 service adguardhome start > /dev/null
